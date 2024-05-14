81ef09d43b832486ff67012364fc6f4694546737
Kriol368 <andreubeltran2004@gmail.com>
2024-05-14 10:39:15 +0200

    feat(master): 14/05/2024 10:39

--- src/main/java/pokedex/App.java
+++ src/main/java/pokedex/App.java
@@ -1,6 +1,6 @@
 package pokedex;
 
-public class PokedexApp {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/pokedex/ui/AppUI.java
@@ -0,0 +1,23 @@
+package pokedex.ui;
+
+import javax.swing.*;
+
+public class AppUI {
+    private JTabbedPane mainPane;
+    private JPanel panel1;
+    private JPanel pokedex;
+    private JPanel team;
+    private JPanel map;
+    private JPanel typeChart;
+    private JPanel network;
+    private JPanel Trainer;
+    private JSplitPane pokedex_split;
+    private JList pokedex_list;
+    private JList teamList;
+    private JSplitPane teamSplit;
+    private JSplitPane team_slots_split;
+    private JSplitPane split_team_1;
+    private JSplitPane split_team_2;
+    private JSplitPane split_team_3;
+    private JSplitPane split_team_4;
+}
