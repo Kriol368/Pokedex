763886191cb69588288f473c2e42739902fc9add
Kriol368 <andreubeltran2004@gmail.com>
2024-05-16 09:03:12 +0200

    feat(master): 16/05/2024 09:03

--- src/main/java/pokedex/repository/Pokemon_types_keyRepository.java
+++
@@ -1,9 +0,0 @@
-package pokedex.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-import pokedex.entity.Pokemon_types_key;
-
-@Component
-public interface Pokemon_types_keyRepository extends CrudRepository<Pokemon_types_key,Integer> {
-}
