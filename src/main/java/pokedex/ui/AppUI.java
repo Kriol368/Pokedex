6d30c9c0061b23e75216cf86cba5037acb70ae5f
Marc Ibàñez <mibanezp0709@gmail.com>
2024-05-20 10:19:36 +0200

    sincePokedex: 20/05/2024 10:19

--- src/main/java/pokedex/ui/AppUI.java
+++ src/main/java/pokedex/ui/AppUI.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import pokedex.repository.*;
 
 import javax.swing.*;
+import java.awt.*;
+
 @Component
 public class AppUI extends JFrame{
     private JTabbedPane mainPane;
@@ -15,7 +17,6 @@ public class AppUI extends JFrame{
     private JPanel typeChart;
     private JPanel network;
     private JPanel Trainer;
-    private JSplitPane pokedex_split;
     private JList pokedex_list;
     private JList teamList;
     private JSplitPane teamSplit;
@@ -45,19 +46,53 @@ public class AppUI extends JFrame{
     private JLabel pokemon4;
     private JLabel pokemon5;
     private JLabel pokemon6;
+    private JPanel listpanel;
+    private JLabel pokemonImage;
+    private JButton registerButton;
+    private JLabel PokemonName;
+    private JLabel Typ1;
+    private JLabel Type2;
+    private JLabel PokedexNumber;
 
     @Autowired
     public AppUI(Pokemon_typesRepository pokemonTypesRepository, PokemonRepository pokemonRepository, RegisterRepository registerRepository, TeamRepository teamRepository, TrainerRepository trainerRepository,TypeRepository typeRepository) {
         setTitle("Pokedex");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1920, 1080);
+        setSize(1280, 720);
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // This line sets the application to fullscreen
         setLocationRelativeTo(null);
         setVisible(true);
         showMainPane();
+        setpokemonTeamImages("1", "1", "1", "1", "1", "1");
+        setTrainerImage("1");
     }
     public void showMainPane(){
         setContentPane(mainPane);
         validate();
         repaint();
+    }
+    private ImageIcon getScaledImage(String imagePath, int width, int height) {
+        ImageIcon icon = new ImageIcon(imagePath);
+        Image img = icon.getImage();
+        Image scaledImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledImg);
+    }
+    public String setPokemonImageIcon(String ordernum){
+        return "C:\\Users\\Miban\\IdeaProjects\\Pokedex\\src\\main\\resources\\official_artwork/normal/" + ordernum + ".png";
+    }
+    public void setpokemonTeamImages (String p1 , String p2, String p3, String p4, String p5, String p6){
+        pokemon1.setIcon(getScaledImage(setPokemonImageIcon(p1), pokemon1.getWidth(), pokemon1.getHeight()));
+        pokemon2.setIcon(getScaledImage(setPokemonImageIcon(p2), pokemon2.getWidth(), pokemon2.getHeight()));
+        pokemon3.setIcon(getScaledImage(setPokemonImageIcon(p3), pokemon3.getWidth(), pokemon3.getHeight()));
+        pokemon4.setIcon(getScaledImage(setPokemonImageIcon(p4), pokemon4.getWidth(), pokemon4.getHeight()));
+        pokemon5.setIcon(getScaledImage(setPokemonImageIcon(p5), pokemon5.getWidth(), pokemon5.getHeight()));
+        pokemon6.setIcon(getScaledImage(setPokemonImageIcon(p6), pokemon6.getWidth(), pokemon6.getHeight()));
+    }
+    public String setTrainerImageIcon(String ordernum){
+        return "C:\\Users\\Miban\\IdeaProjects\\Pokedex\\src\\main\\resources\\trainers/" + ordernum + ".png";
+    }
+    public void setTrainerImage (String t1){
+        characterImage.setIcon(getScaledImage(setTrainerImageIcon(t1), characterImage.getWidth(), characterImage.getHeight()));
+
     }
 }
